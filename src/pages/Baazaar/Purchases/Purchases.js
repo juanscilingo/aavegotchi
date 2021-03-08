7f03faff61ec418edbdfa943725a32872a1cf819
Juan Manuel Scilingo <juanscilingo@gmail.com>
2021-03-08 19:39:53 -0300

    Added highlights to purchases page

--- src/pages/Baazaar/Purchases/Purchases.js
+++ src/pages/Baazaar/Purchases/Purchases.js
@@ -1,20 +1,25 @@
 import { AgGridColumn } from "ag-grid-react/lib/agGridColumn";
 import { AgGridReact } from "ag-grid-react/lib/agGridReact";
+import BigNumber from "bignumber.js";
 import Loader from "components/UI/Loader/Loader";
+// import { sub } from "date-fns";
 import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import { LISTING_CATEGORY_NAME } from "utils/constants";
+import { LISTING_CATEGORY, LISTING_CATEGORY_NAME } from "utils/constants";
 import { diamondContract } from "utils/contracts";
 import formatter from "utils/formatter";
 import { TOKENS } from "utils/tokens";
 import web3 from "utils/web3";
+import Highlight from "./components/Highlight";
 
 const Style = styled.div`
 
 `
 
 const Highlights = styled.div`
-  margin-bottom: 30px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-wrap: wrap;
 `
 
 const Grid = styled.div`
@@ -48,14 +53,57 @@ const buildRequests = (latestBlock, sinceBlock) => {
 
 const mapEvent = event => event.returnValues;
 
+const getHighestPricePurchase = data => data.reduce((prev, current) => (new BigNumber(prev.priceInWei).isGreaterThan(new BigNumber(current.priceInWei))) ? prev : current);
+const getLowestPricePurchase = data => data.reduce((prev, current) => (new BigNumber(prev.priceInWei).isLessThan(new BigNumber(current.priceInWei))) ? prev : current);
+const getAveragePurchasePrice = (data, category) => {
+  const filteredData = data.filter(d => d.category === category.toString());
+
+  if (!filteredData.length)
+    return;
+
+  const totalSpent = filteredData.reduce((acc, purchase) => acc.plus(new BigNumber(purchase.priceInWei)), new BigNumber(0));
+  return totalSpent.dividedBy(filteredData.length);
+}
+
 const Purchases = props => {
   const [purchases, setPurchases] = useState();
   const [highlights, setHighlights] = useState();
   const latestBlock = useRef();
 
-  // const computeHighlights = data => {
-  //   const highestPrice = data.reduce((prev, current) => (prev.priceInWei > current.priceInWei) ? prev : current)
-  // }
+  const computeHighlights = data => {
+    const highlights = [];
+
+    // ALL TIME
+    const highestPricePurchase = getHighestPricePurchase(data);
+    if (highestPricePurchase) highlights.push({ key: 'highest-price-purchase', title: 'Highest Paid', subtitle: LISTING_CATEGORY_NAME[highestPricePurchase.category], value: formatter.token(highestPricePurchase.priceInWei, TOKENS.GHST), href: `https://aavegotchi.com/baazaar/erc721/${highestPricePurchase.listingId}` });
+    
+    const lowestPricePurchase = getLowestPricePurchase(data);
+    if (lowestPricePurchase) highlights.push({ key: 'lowest-price-purchase', title: 'Lowest Paid', subtitle: LISTING_CATEGORY_NAME[lowestPricePurchase.category], value: formatter.token(lowestPricePurchase.priceInWei, TOKENS.GHST), href: `https://aavegotchi.com/baazaar/erc721/${lowestPricePurchase.listingId}` });
+    
+    const averageClosedPortalPurchasePrice = getAveragePurchasePrice(data, LISTING_CATEGORY["portal"]);
+    if (averageClosedPortalPurchasePrice) highlights.push({ key: 'average-closed-portal-purchase-price', title: 'Avg. Purchase Price', subtitle: 'Closed Portal', value: formatter.token(averageClosedPortalPurchasePrice.toString(), TOKENS.GHST) });
+
+    const averageOpenPortalPurchasePrice = getAveragePurchasePrice(data, LISTING_CATEGORY["open-portal"]);
+    if (averageOpenPortalPurchasePrice) highlights.push({ key: 'average-open-portal-purchase-price', title: 'Avg. Purchase Price', subtitle: 'Open Portal', value: formatter.token(averageOpenPortalPurchasePrice.toString(), TOKENS.GHST) });
+    
+    const averageAavegotchiPurchasePrice = getAveragePurchasePrice(data, LISTING_CATEGORY["aavegotchi"]);
+    if (averageAavegotchiPurchasePrice) highlights.push({ key: 'average-aavegotchi-purchase-price', title: 'Avg. Purchase Price', subtitle: 'Aavegotchi', value: formatter.token(averageAavegotchiPurchasePrice.toString(), TOKENS.GHST) });
+
+    // LAST 7 DAYS
+    // const since = sub(new Date(), { days: 7 });
+    // const last7DaysData = data.filter(d => new Date(d.time * 1000) >= since);
+    // const highestPricePurchase7Days = getHighestPricePurchase(last7DaysData);
+    // const lowestPricePurchase7Days = getLowestPricePurchase(last7DaysData);
+    // const averagePurchasePrice7Days = getAveragePurchasePrice(last7DaysData);
+
+    // const highlights = [
+      // { key: '7-day-highest-price-purchase', title: '7-Day Highest Price Purchase', value: formatter.token(highestPricePurchase7Days.priceInWei, TOKENS.GHST) },
+      // { key: '7-day-lowest-price-purchase', title: '7-Day Lowest Price Purchase', value: formatter.token(lowestPricePurchase7Days.priceInWei, TOKENS.GHST) },
+      // { key: '7-day-average-purchase-price', title: '7-Day Average Purchase Price', value: formatter.token(averagePurchasePrice7Days.toString(), TOKENS.GHST) },
+    // ];
+
+    setHighlights(highlights);
+  }
 
   useEffect(() => {
     const getPastEvents = async () => {
@@ -80,10 +128,10 @@ const Purchases = props => {
   }, []);
 
   useEffect(() => {
-    if (!!latestBlock.current) {
-      setHighlights([])
+    if (!!latestBlock.current && !highlights) {
+      computeHighlights(purchases);
     }
-  }, [purchases])
+  }, [purchases, highlights])
 
   if (!purchases)
     return null;
@@ -92,16 +140,19 @@ const Purchases = props => {
     api.sizeColumnsToFit();
   }
 
-  // const onFilterChanged = ({ api }) => {
-  //   const filteredRows = api.rowModel.rowsToDisplay.map(row => row.data);
-  //   console.log('filter changed...', filteredRows)
-  // }
+  const onFilterChanged = ({ api }) => {
+    const filteredRows = api.rowModel.rowsToDisplay.map(row => row.data);
+    console.log('filter changed...', filteredRows)
+    computeHighlights(filteredRows);
+  }
 
   return (
     <Style>
       {highlights ? (
         <Highlights>
-          {/* <div>Highlights!</div> */}
+          {highlights.map(highlight => (
+            <Highlight {...highlight} />
+          ))}
         </Highlights>
       ) : (
         <Loader />
@@ -114,7 +165,7 @@ const Purchases = props => {
           pagination 
           paginationAutoPageSize 
           enableCellTextSelection
-          // onFilterChanged={onFilterChanged}
+          onFilterChanged={onFilterChanged}
         >
           <AgGridColumn field="time" sort="desc" headerName="Date" valueFormatter={({ value }) => new Date(value * 1000).toLocaleString()}></AgGridColumn>
           <AgGridColumn field="category" filterValueGetter={({ data }) => LISTING_CATEGORY_NAME[data.category]} valueFormatter={({ value }) => LISTING_CATEGORY_NAME[value]}></AgGridColumn>
---
+++ src/pages/Baazaar/Purchases/components/Highlight.js
@@ -0,0 +1,53 @@
+import styled, { css } from "styled-components";
+
+const container = css`
+  padding: 15px;
+  background: var(--purple-2);
+  box-shadow: var(--box-shadow);
+  border-radius: var(--border-radius);
+  margin-right: 15px;
+  margin-bottom: 15px;
+`
+
+const Container = styled.div`
+  ${container}
+`
+
+const TitleContainer = styled.div`
+  margin-bottom: 10px;
+  min-height: 32px;
+`
+
+const Title = styled.div`
+  font-weight: 500;
+  font-size: 14px;
+`
+
+const Subtitle = styled.div`
+  font-size: 12px;
+  color: lightgray;
+`
+
+const Value = styled.div`
+  font-weight: 600;
+  font-size: 14px;
+`
+
+const LinkContainer = styled.a`
+  ${container}
+`
+
+const Highlight = props => {
+  const Wrapper = props.href ? LinkContainer : Container; 
+  return (
+    <Wrapper href={props.href} target="_blank" rel="noopener noreferrer">
+      <TitleContainer>
+        <Title>{props.title}</Title>
+        <Subtitle>{props.subtitle}</Subtitle>
+      </TitleContainer>
+      <Value>{props.value}</Value>
+    </Wrapper>
+  )
+}
+
+export default Highlight;
